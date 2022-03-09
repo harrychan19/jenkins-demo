6f557c0b45a13c5b173e213efe4f8d74c1544c80
shihaihu <hushihai@ihuman.com>
2022-03-09 22:16:18 +0800

    Initial commit

---
+++ src/main/java/com/hsh/jenkinsdemo/JenkinsDemoApplication.java
@@ -0,0 +1,14 @@
+package com.hsh.jenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class JenkinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDemoApplication.class, args);
+    }
+
+}
